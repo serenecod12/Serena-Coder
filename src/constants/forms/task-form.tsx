0948d2d5efc7aeddcbd4ae00712398d1f5ffda74
Abulfayz Saidahmadov <saidahmadovfayz@gmail.com>
2025-06-11 16:45:39 +0500

    TypeScript xatoliklarini tuzatildi

--- src/constants/forms/task-form.tsx
+++ src/constants/forms/task-form.tsx
@@ -18,62 +18,56 @@ import { useUserState } from '@/stores/user.store'
 import { toast } from 'sonner'
 
 interface Props {
-	title? : string
-	isEdid? : boolean
-	onClosed? : () => void
-	hander : (values: z.infer<typeof formSchema>) => Promise<void>
+	// title va isEdit ishlatilmasa, ularsiz qoldirish mumkin yoki ishlatiladigan qilib yozish kerak
+	title?: string
+	isEdid?: boolean
+	onClosed?: () => void
+	handler: (values: z.infer<typeof formSchema>) => Promise<void>
 }
 
-const TaskForm = ({title='', handler} : Props) => {
-
+const TaskForm = ({ title = '', isEdid, onClosed, handler }: Props) => {
 	const [isLoading, setIsLoading] = useState(false)
-
-	const {user} = useUserState()
-
+	const { user } = useUserState()
 
 	const form = useForm<z.infer<typeof formSchema>>({
-			resolver: zodResolver(formSchema),
-			defaultValues: { title : "" },
+		resolver: zodResolver(formSchema),
+		defaultValues: { title: '' },
+	})
+
+	const onSubmit = async (values: z.infer<typeof formSchema>) => {
+		if (!user) return null
+
+		setIsLoading(true)
+
+		const promise = handler(values).finally(() => setIsLoading(false))
+
+		toast.promise(promise, {
+			loading: "Kutamiz!!!",
+			success: "Bajarildi!!!",
+			error: "Uzur, qila olmayman!!"
 		})
-
-		const onSubmit = async (values: z.infer<typeof formSchema>) => {
-			if(!user) return null
-			const {title} = values
-			setIsLoading(true)
-
-			const promise = handler(values).finally(()=> setIsLoading(false))
-
-			toast.promise(promise, {
-				loading : "Kutamiz!!!",
-				success : "Bajarildi!!!",
-				error : "Uzur, qila olmayman!!"
-			})
-		}
-
+	}
 
 	return (
-		<>
-	    	<Form {...form}>
-	    	<form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-	    		<FormField
-	    			control={form.control}
-	    			name="title"
-	    			render={({ field }) => (
-	    				<FormItem>
-	    					<FormLabel>Ilova yoki sayt uchun: Foydalanuvchi nomi</FormLabel>
-	    					<FormControl>
-	    						<Input placeholder="shadcn" {...field} disabled = {isLoading} />
-	    					</FormControl>
-	    					<FormMessage />
-	    				</FormItem>
-	    			)}
-	    		/>
-	    		<Button type="submit" disabled ={isLoading} > Tasdiqlash</Button>
-	    	</form>
-	    </Form>
-		</>
-)
-	
+		<Form {...form}>
+			<form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+				<FormField
+					control={form.control}
+					name="title"
+					render={({ field }) => (
+						<FormItem>
+							<FormLabel>Ilova yoki sayt uchun: Foydalanuvchi nomi</FormLabel>
+							<FormControl>
+								<Input placeholder="shadcn" {...field} disabled={isLoading} />
+							</FormControl>
+							<FormMessage />
+						</FormItem>
+					)}
+				/>
+				<Button type="submit" disabled={isLoading}>Tasdiqlash</Button>
+			</form>
+		</Form>
+	)
 }
 
 export default TaskForm
